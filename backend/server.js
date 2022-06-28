00ac530dc636769d1cfc843083517083c43cd251
h4lling <info@idahalling.com>
2022-06-28 10:44:38 +0200

    added endpoints list

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,15 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import { isEmail } from "validator";
+import getEndpoints from "express-list-endpoints";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
-//
-// Add middlewares to enable cors and json body parsing
+
 app.use(cors());
 app.use(express.json());
 
@@ -28,9 +25,8 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", UserSchema);
 
-// Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(getEndpoints(app));
 });
 
 app.post("/signup", async (req, res) => {
@@ -51,7 +47,6 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });

b0eba274f23ee8ee9fbca45d7aec2f45bf8fb236
h4lling <info@idahalling.com>
2022-08-17 10:55:34 +0200

    added menu posts and arrow for navigation

--- frontend/src/components/Arrow.js
+++ frontend/src/components/Arrow.js
@@ -1,53 +1,15 @@
-import styled from "styled-components";
 import ScrollIntoView from "react-scroll-into-view";
-const Wrapper = styled.div`
-  position: absolute;
-  top: 90%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 2;
-`;
+import { ArrowDown, ArrowWrapper } from "theme/styles";
 
-const ArrowDown = styled.div`
-  .span {
-    display: block;
-    width: 20px;
-    height: 20px;
-    border-bottom: 5px solid #edbe44;
-    border-right: 5px solid #edbe44;
-    transform: rotate(45deg);
-    margin: -10px;
-    animation: animate 3s infinite;
-  }
-  .arrow span:nth-child(2) {
-    animation-delay: -0.2s;
-  }
-  .arrow span:nth-child(3) {
-    animation-delay: -0.4s;
-  }
-  @keyframes animate {
-    0% {
-      opacity: 0;
-      transform: rotate(45deg) translate(-20px, -20px);
-    }
-    50% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 0;
-      transform: rotate(45deg) translate(20px, 20px);
-    }
-  }
-`;
 export const Arrow = () => {
   return (
-    <Wrapper>
+    <ArrowWrapper>
       <ScrollIntoView selector="#history-quote">
         <ArrowDown className="arrow">
           <div className="span"></div>
           <div className="span"></div>
         </ArrowDown>
       </ScrollIntoView>
-    </Wrapper>
+    </ArrowWrapper>
   );
 };

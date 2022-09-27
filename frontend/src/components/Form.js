643acdeae1daae11eb283a11d0b92fceebc36a4f
h4lling <halling.ida@gmail.com>
2022-09-27 10:19:49 +0200

    added hej

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -60,6 +60,7 @@ export const Form = ({ showThankYou, setShowThankYou }) => {
             {" "}
             Vad kul att du vill veta mer, <br />
             du kommer få ett mail när boken släpps.
+            <p>hej</p>
           </h3>
         </StyledThankYou>
       )}

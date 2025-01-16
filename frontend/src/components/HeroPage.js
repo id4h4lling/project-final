d8e1ef52cee95b20a0c37bd0208507c52df88d9e
h4lling <halling.ida@gmail.com>
2025-01-16 12:09:43 +0100

    sv bokkonst

--- frontend/src/components/HeroPage.js
+++ frontend/src/components/HeroPage.js
@@ -23,6 +23,7 @@ export const HeroPage = ({ hero }) => {
             <h4>{hero.topheading}</h4>
             <h1>{hero.mainHeader}</h1>
             <h3>{hero.subheading}</h3>
+            <h4>Vinnare av Svensk Bokkonst 2023</h4>
           </div>
           <Arrow />
         </div>
